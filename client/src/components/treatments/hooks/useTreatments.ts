2cef319b11fecbec25d6f7111f4e51172340ff97
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-08-05 07:07:04 +0200

    prefetch data from the server via useQueryClient and store it in the cache

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -1,5 +1,5 @@
 import type { Treatment } from '@shared/types';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { axiosInstance } from '@/axiosInstance';
 import { queryKeys } from '@/react-query/constants';
@@ -18,4 +18,13 @@ export function useTreatments(): Treatment[] {
     queryFn: getTreatments,
   });
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  // prefetch data from the server via useQueryClient and store it in the cache
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery({
+    queryKey: [queryKeys.treatments],
+    queryFn: getTreatments,
+  });
 }
